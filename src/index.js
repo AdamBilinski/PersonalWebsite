0d91591420b942c4327f59fea463946d6b93c616
Adam Bilinski <adam.bilinski@ihsmarkit.com>
2019-01-31 15:05:33 +0000

    Added navigation and couple of simple pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './website/index.css';
+import Main from "./website/Main";
 import * as serviceWorker from './serviceWorker';
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
